6419b5533b8953bb59a40a84ec91439b33de7187
Morgan Donze <donzemorgan@gmail.com>
2023-04-15 10:18:29 -0400

    Refactor truck script

--- apps/punchy_api/priv/populate_trucks_operations.exs
+++ apps/punchy_api/priv/populate_trucks_operations.exs
@@ -4,54 +4,56 @@ NimbleCSV.define(CSVParser, separator: ",", escape: "\"")
 {:ok, cwd} = File.cwd()
 truck_data_save_path = cwd <> "/priv/truck_data.csv"
 
+defmodule RowProcessor do
+  def process_row(row) do
+    data_map = [
+      {1, :truck_name},
+      {2, :facility_type},
+      {4, :location_description},
+      {5, :address},
+      {11, :food_items},
+      {14, :latitude},
+      {15, :longitude},
+      {17, :days_hours}
+    ]
+
+    keep_cols = Enum.map(data_map, fn {index, _column} -> index end)
+
+    add_index = fn elem, index ->
+      {index, elem}
+    end
+
+    keep_col? = fn {index, _elem} ->
+      Enum.member?(keep_cols, index)
+    end
+
+    find_key = fn {index, data_map_index, _key} ->
+      data_map_index == index
+    end
+
+    set_attr = fn {index, value}, acc ->
+      {_data_map_index, key} =
+        Enum.find(data_map, fn {data_map_index, _key} ->
+          data_map_index == index
+        end)
+
+      Map.put(acc, key, value)
+    end
+
+    row
+    |> Enum.with_index(add_index)
+    |> Enum.filter(keep_col?)
+    |> Enum.reduce(%{}, set_attr)
+  end
+end
+
 # case Download.from(truck_data_url, [path: truck_data_save_path]) do
 case {:ok, truck_data_save_path} do
   {:ok, path} ->
-    process_row = fn row ->
-      data_map = [
-        {1, :truck_name},
-        {2, :facility_type},
-        {4, :location_description},
-        {5, :address},
-        {11, :food_items},
-        {14, :latitude},
-        {15, :longitude},
-        {17, :days_hours}
-      ]
-
-      keep_cols = Enum.map(data_map, fn {index, _column} -> index end)
-
-      add_index = fn elem, index ->
-        {index, elem}
-      end
-
-      keep_col? = fn {index, _elem} ->
-        Enum.member?(keep_cols, index)
-      end
-
-      find_key = fn {index, data_map_index, _key} ->
-        data_map_index == index
-      end
-
-      set_attr = fn {index, value}, acc ->
-        {_data_map_index, key} =
-          Enum.find(data_map, fn {data_map_index, _key} ->
-            data_map_index == index
-          end)
-
-        Map.put(acc, key, value)
-      end
-
-      row
-      |> Enum.with_index(add_index)
-      |> Enum.filter(keep_col?)
-      |> Enum.reduce(%{}, set_attr)
-    end
-
     path
     |> File.stream!()
     |> CSVParser.parse_stream()
-    |> Stream.map(process_row)
+    |> Stream.map(&RowProcessor.process_row/1)
     |> Enum.take(1)
     |> IO.inspect()
 
