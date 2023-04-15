83904dd998d950eab313b4aa95070f6efc1f246c
Morgan Donze <donzemorgan@gmail.com>
2023-04-15 07:00:45 -0400

    Start work on nearby operations method

--- apps/punchy_api/lib/punchy_api.ex
+++ apps/punchy_api/lib/punchy_api.ex
@@ -2,17 +2,30 @@ defmodule PunchyApi do
   @moduledoc """
   Documentation for `PunchyApi`.
   """
+  alias PunchyApi.Location
 
   @doc """
-  Hello world.
 
-  ## Examples
+  Gets operations near the time and place specified by the params.
 
-      iex> PunchyApi.hello()
-      :world
+  ## Params
+
+  - latitude
+  - longitude
+  - datetime
 
   """
-  def hello do
-    :world
+  def get_nearby_operations(latitude, longitude, datetime) do
+    nearby_radius_in_miles = 2
+    approx_miles_per_degree = 25_000 / 360
+    mile_in_degrees = 1 / approx_miles_per_degree
+
+    min_lat = latitude - nearby_radius_in_miles * mile_in_degrees
+    max_lat = latitude + nearby_radius_in_miles * mile_in_degrees
+    min_long = longitude - nearby_radius_in_miles * mile_in_degrees
+    max_long = longitude + nearby_radius_in_miles * mile_in_degrees
+
+    # Use min/max lat/long in query to select nearby Operations
+
   end
 end
